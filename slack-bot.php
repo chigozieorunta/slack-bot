157403f772ecb5de8e3eb911a7e006676ba23192
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-09-26 04:30:59 +0100

    Added plugin file

---
+++ slack-bot.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Slack Bot
+ * Description: Slack Bot for WordPress.
+ * Version: 1.0.0
+ * Author: Chigozie Orunta
+ * Author URI: https://github.com/chigozieorunta/slack-bot
+ * Text Domain: slack-bot
+ *
+ * @package SlackBot
+ */
