e77d9df995320132afff25e4d3c35fc661e6b905
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-09-29 17:35:22 +0100

    Declared plugin entry point based on Singleton pattern

--- src/Plugin.php
+++ src/Plugin.php
@@ -20,4 +20,17 @@ class Plugin {
 	public function __construct() {
 		//...
 	}
+
+	/**
+	 * Plugin Entry point based on Singleton
+	 *
+	 * @return Plugin $plugin Instance of the plugin abstraction.
+	 */
+	public static function init() {
+		static $instance;
+		if ( ! $instance === null ) {
+			$instance = new self();
+		}
+		return instance;
+	}
 }
