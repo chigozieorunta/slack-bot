d91386c32365311b4bc4256f5ddf2a1f632e3069
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-09-29 17:29:33 +0100

    Added Plugin base class

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin class.
+ *
+ * @package SlackBot
+ */
+
+namespace SlackBot;
+
+/**
+ * WordPress plugin interface.
+ */
+class Plugin {
+
+	/**
+	 * Setup the plugin.
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		//...
+	}
+}
