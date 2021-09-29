43814e7a691a8b1f9ce045fbb5138deb3d5f72fa
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-09-29 17:36:39 +0100

    Support for site level autoloading

--- slack-bot.php
+++ slack-bot.php
@@ -9,3 +9,8 @@
  *
  * @package SlackBot
  */
+
+ // Support for site-level autoloading.
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
