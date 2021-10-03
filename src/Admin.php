1c4cdfa6e81a47953a6e465963a4db778cd1e550
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-03 04:17:28 +0100

    Instantiate class for Admin page

---
+++ src/Admin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Admin class.
+ *
+ * @package SlackBot
+ */
+
+namespace SlackBot;
+
+/**
+ * Admin Page interface
+ */
+class Admin {
+
+	/**
+	 * Instantiate class
+	 *
+	 * @return void
+	 */
+	public function __construct() {
+		//..
+	}
+}
