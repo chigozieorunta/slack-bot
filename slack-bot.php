14145750d43ceb722d20fd65c8927b12c5b530d3
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-05 16:43:11 +0100

    Fixed typo error on plugin use case

--- slack-bot.php
+++ slack-bot.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Slack Bot
- * Description: Notify your Slack channel when posts, pages, CPTs have been created or published on your WordPress site.
+ * Description: Notify your Slack channel when posts have been created or published on your WordPress site.
  * Version: 1.0.0
  * Author: Chigozie Orunta
  * Author URI: https://github.com/chigozieorunta/slack-bot
