b6ef39f7021d5029e7f199266ee75bfcec355953
Chigozie Orunta <chigozieorunta@yahoo.com>
2021-10-31 01:44:57 +0100

    Dependencies can now be packaged for plugin via Gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const gulp = require('gulp');
 
 gulp.task('build', async function () {
@@ -9,8 +10,21 @@ gulp.task('build', async function () {
 		.pipe(gulp.dest('build'));
 	gulp.src('./readme.txt')
 		.pipe(gulp.dest('build'));
-	gulp.src('assets/*.')
+	gulp.src('assets/*')
 		.pipe(gulp.dest('build/assets'));
-	gulp.src('languages/*.')
+	gulp.src('languages/*')
 		.pipe(gulp.dest('build/languages'));
+	vendor();
 });
+
+const vendor = () => {
+	const composer = fs.readFileSync('./composer.json', 'utf8');
+	const dependencies = Object.keys(JSON.parse(composer).require);
+	dependencies.forEach((key, index) => {
+		let dependency = key.split('/');
+		if(dependency[0] !== 'php') {
+			gulp.src(`vendor/${dependency[0]}/**/*`)
+				.pipe(gulp.dest(`build/vendor/${dependency[0]}`));
+		}
+	});
+}
